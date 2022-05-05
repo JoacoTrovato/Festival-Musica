a261208e3c936c49dca06d4d1ea9956de1bd9787
JoacoTrovato <joacotrovato@gmail.com>
2022-05-05 14:25:30 -0300

    First Commit

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+const { src, dest, watch } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const plumber = require("gulp-plumber");
+
+function css( done ) {
+    src("src/scss/**/*.scss") // Identificar el archivo .SCSS a compilar
+        .pipe( plumber() )
+        .pipe( sass() ) // Compilarlo
+        .pipe( dest("build/css") ) // Almacenarlo en el disco duro
+    done(); // Callback que avisa a gulp cuando llegamos al final
+}
+
+function dev( done ) {
+    watch("src/scss/**/*.scss", css)
+    done();
+}
+
+exports.css = css;
+exports.dev = dev;
