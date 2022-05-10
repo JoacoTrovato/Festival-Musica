9075bd2dcb4741f608c8b87eeeef4c4f073e3e27
JoacoTrovato <joacotrovato@gmail.com>
2022-05-10 16:50:40 -0300

    Progreso

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,14 @@
-const { src, dest, watch } = require("gulp");
+const { src, dest, watch, parallel } = require("gulp");
+
+// CSS
 const sass = require("gulp-sass")(require("sass"));
 const plumber = require("gulp-plumber");
+
+// Imágenes
+const cache = require("gulp-cache");
+const imagemin = require("gulp-imagemin");
+const webp = require("gulp-webp");
+const avif = require("gulp-avif");
 
 function css( done ) {
     src("src/scss/**/*.scss") // Identificar el archivo .SCSS a compilar
@@ -10,10 +18,51 @@ function css( done ) {
     done(); // Callback que avisa a gulp cuando llegamos al final
 }
 
+function imagenes( done ) {
+    const options = {
+        optimizationLevel: 3
+    }
+    src("src/img/**/*.{png,jpg}")
+        .pipe( cache( imagemin(options) ) )
+        .pipe( dest("build/img") )
+    done();
+}
+
+function versionWebp( done ) {
+    const options = {
+        quality: 50
+    };
+    src("src/img/**/*.{png,jpg}")
+        .pipe( webp(options) )
+        .pipe( dest("build/img") )
+    done();
+}
+
+function versionAvif( done ) {
+    const options = {
+        quality: 50
+    };
+    src("src/img/**/*.{png,jpg}")
+        .pipe( avif(options) )
+        .pipe( dest("build/img") )
+    done();
+}
+
+function javascript( done ) {
+    src("src/js/**/*.js")
+        .pipe(dest("build/js"));
+    done();
+}
+
 function dev( done ) {
-    watch("src/scss/**/*.scss", css)
+    watch("src/scss/**/*.scss", css);
+    watch("src/js/**/*.js", javascript);
     done();
 }
 
 exports.css = css;
-exports.dev = dev;
+exports.javascript = javascript;
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
+exports.dev = parallel(imagenes, versionWebp, versionAvif, javascript, dev);
